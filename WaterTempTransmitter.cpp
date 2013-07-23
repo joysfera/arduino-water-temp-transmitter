d071a78e29ffc6f0895fac40854a194ebc851f7a
Petr Stehlík <petr@pstehlik.cz>
2013-07-23 18:58:46 +0200

    improved but not working versions

---
+++ WaterTempTransmitter.cpp
@@ -0,0 +1,78 @@
+/************
+ * WaterTempTransmitter
+ ************/
+
+// Author: Abdullah Tahiri
+// http://alejandro-anv.blogspot.cz/2012/12/hackeando-termometro-inalambrico.html
+// Code taken from comment below 
+// http://dephiox.blogspot.cz/2012/12/i-recently-stumbeled-over-433-mhz.html
+
+#include "WaterTempTransmitter.h"
+
+WaterTempTransmitter::WaterTempTransmitter(byte pin, byte id, byte channel)
+{
+    _pin=pin;
+    _id=id;
+    _chan=(channel & 0x03); // it is just two bits (never trust the user ! ;-P)
+    pinMode(_pin, OUTPUT);
+}
+
+void WaterTempTransmitter::send(float temperature, bool battery, bool beep)
+{
+    int16_t temp= (int16_t)(temperature * 10);
+    unsigned long packet= _id ;
+    packet<<= 12;
+    packet|= (temp & 0x8000)==0x8000?((temp & 0x07FF) | 0x0800):(temp & 0x07FF);
+    packet<<= 2;
+    packet|=_chan;
+    packet<<=2;
+    packet|= (battery==false)?0x2:00;
+    packet|= (beep==true)?0x1:00;
+
+    // Checksum (summatory of nibbles +1)
+    unsigned long temppacket=packet;
+    byte checksum= (byte)temppacket;
+    for(byte i=1;i<7;i++)
+        checksum+=(temppacket >>= 4);
+    checksum-=1;
+
+    temppacket=checksum; // variable reuse
+    temppacket<<= 24;
+    packet|=temppacket;
+
+    sendFrame(_pin,packet,WTT_BITS,WTT_REPETITIONS);
+}
+
+void WaterTempTransmitter::sendPacket(byte pin, unsigned long packet, byte nbits)
+{
+    //send preamble
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(WTT_PULSEDURATION);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(WTT_PREAMBLE);
+
+    for(byte i=(nbits-1);i>=0;i--)
+    {
+        if( (packet >> i) & 0x00000001)
+        {// send 1
+            digitalWrite(pin, HIGH);
+            delayMicroseconds(WTT_PULSEDURATION);
+            digitalWrite(pin, LOW);
+            delayMicroseconds(WTT_ONE);
+        } 
+        else
+        {// send 0
+            digitalWrite(pin, HIGH);
+            delayMicroseconds(WTT_PULSEDURATION);
+            digitalWrite(pin, LOW);
+            delayMicroseconds(WTT_ZERO); 
+        }
+    }
+}
+
+void WaterTempTransmitter::sendFrame(byte pin, unsigned long packet, byte nbits, byte repetitions)
+{
+    for(byte j=0;j<repetitions;j++)
+        sendPacket(pin, packet, nbits);
+}
+
---
+++ WaterTempTransmitter.h
@@ -0,0 +1,38 @@
+//********************* WaterTempTransmitter.h **************//
+
+#ifndef WaterTempTransmitter_h
+#define WaterTempTransmitter_h
+
+#if (ARDUINO >= 100)
+    #include <Arduino.h>
+#else
+    #include <WProgram.h>
+#endif
+
+#define WTT_PULSEDURATION 500
+#define WTT_ZERO 2000
+#define WTT_ONE 4500
+#define WTT_PREAMBLE 9500
+#define WTT_BITS 28
+#define WTT_REPETITIONS 8
+
+class WaterTempTransmitter {
+public:
+    // pin is the pin connected to the data of the 433 transmitter
+    // id is an 8 bit identifier (you can create your own)
+    // The channel which can be (1,2,3)
+    WaterTempTransmitter(byte pin, byte id, byte channel);
+
+    void send(float temperature, bool battery=false, bool beep=false);
+
+protected:
+    void sendPacket(byte pin, unsigned long packet, byte nbits);
+    void sendFrame(byte pin, unsigned long packet, byte nbits, byte repetitions);
+protected:
+    byte _pin; 
+    byte _id; 
+    byte _chan;
+};
+
+#endif
+
